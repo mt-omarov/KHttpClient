49d1763624305d03c1cd2209962eec3dfccd9cf3
Marat Omarov <marat@192.168.1.6>
2023-05-26 00:56:56 +0300

    Ported a runningResponses class with its methods

--- src/HttpClient/Components/RunningResponses.php
+++ src/HttpClient/Components/RunningResponses.php
@@ -5,7 +5,7 @@ use Kaa\HttpClient\Components\CurlClientState;
 
 class RunningResponses
 {
-    public CurlClientState $multi;
+    private CurlClientState $multi;
     /** @var array<int, CurlResponse> $responses */
     public array $responses;
 
@@ -15,8 +15,31 @@ class RunningResponses
         $this->responses = [];
     }
 
-    public function setResponse(int $id, CurlResponse $response)
+    public function setResponse(int $id, CurlResponse $response): self
     {
         $this->responses[$id] = $response;
+        return $this;
+    }
+
+    /**
+     * @return CurlResponse[]
+     */
+    public function getResponses(): array
+    {
+        return $this->responses;
+    }
+
+    public function getResponse(int $key): ?CurlResponse
+    {
+        if (isset($this->responses[$key])){
+            return $this->responses[$key];
+        } else {
+            return null;
+        }
+    }
+
+    public function getMulti(): CurlClientState
+    {
+        return $this->multi;
     }
 }
