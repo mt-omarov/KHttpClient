f9d014fcca293b6ba7b4c45428073a18b8838abb
Marat Omarov <marat@192.168.1.3>
2023-05-30 19:16:06 +0300

    Fixed errors with namespase

--- src/HttpClient/Contracts/ResponseInterface.php
+++ src/HttpClient/Contracts/ResponseInterface.php
@@ -1,13 +1,12 @@
 <?php
 
-namespace kCurl\HttpClient\Contracts;
-
+namespace Kaa\HttpClient\Contracts;
 interface ResponseInterface
 {
     public function getStatusCode(): int;
     public function getHeaders(bool $throw = true): array;
     public function getContent(bool $throw = true): string;
-    public function toArray(bool $throw = true): array;
-    public function cancel(): void;
-    public function getInfo(string $type = null): mixed;
+    //public function toArray(bool $throw = true): array;
+    //public function cancel(): void;
+    public function getInfo(?string $type = null);
 }
